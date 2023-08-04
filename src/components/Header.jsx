3219627d6a2fb050cff3494b0fe4056266ecb784
jadiel07 <jadiel174@hotmail.com>
2023-08-04 13:31:23 -0300

    instalação do tailwind e add do componente header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul className="flex justify-center gap-5">
+          <li>Skills</li>
+          <li>Home</li>
+          <li>Sobre-mim</li>
+          <li>Contatos</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
