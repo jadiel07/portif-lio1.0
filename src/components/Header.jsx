9c32ca239a6b0e8fb51a033e2b4dfcca221a6b2b
jadiel07 <jadiel174@hotmail.com>
2023-08-05 20:34:56 -0300

    Criação do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,44 @@ export default function Header() {
   return (
     <header>
       <nav>
-        <ul className="flex justify-center gap-5">
-          <li>Skills</li>
-          <li>Home</li>
-          <li>Sobre-mim</li>
-          <li>Contatos</li>
+        <ul className="flex justify-center  m-1 sm:gap-4 md:gap-6 lg:gap-10">
+          <li
+            className=" font-bold cursor-pointer
+           bg-gradient-to-r text-transparent bg-clip-text from-orange-400 to-orange-600
+           hover:from-orange-600 hover:to-orange-700 transition-all hover:border-b-2  hover:border-orange-600
+           sm:text-2xl md:text-3xl lg:text-4xl
+           "
+          >
+            Skills
+          </li>
+          <li
+            className=" font-bold
+            cursor-pointer
+            bg-gradient-to-r text-transparent bg-clip-text from-orange-400 to-orange-600
+            hover:from-orange-600 hover:to-orange-700 transition-all hover:border-b-2  hover:border-orange-600
+            sm:text-2xl md:text-3xl lg:text-4xl
+            "
+          >
+            Home
+          </li>
+          <li
+            className=" font-bold cursor-pointer
+            bg-gradient-to-r text-transparent bg-clip-text from-orange-400 to-orange-600
+            hover:from-orange-600 hover:to-orange-700 transition-all hover:border-b-2  hover:border-orange-600
+            sm:text-2xl md:text-3xl lg:text-4xl
+            "
+          >
+            Contatos
+          </li>
+          <li
+            className=" font-bold cursor-pointer
+            bg-gradient-to-r text-transparent bg-clip-text from-orange-400 to-orange-600
+            hover:from-orange-600 hover:to-orange-700 transition-all hover:border-b-2  hover:border-orange-600
+            sm:text-2xl md:text-3xl lg:text-4xl
+            "
+          >
+            Sobre-mim
+          </li>
         </ul>
       </nav>
     </header>
