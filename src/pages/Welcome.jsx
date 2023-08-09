4ed3d2580a5a21d762c0c08f387435c5e9e8f102
jadiel07 <jadiel174@hotmail.com>
2023-08-09 17:41:05 -0300

    Atualização do home

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
-import Header from "../components/Header";
+import IconsHome from "../components/home/IconsHome";
 
 export default function Welcome() {
   return (
-    <main className="grid place-items-center ">
-      <Header />
+    <main className="h-screen grid place-items-center ">
+      <h1>Hello, welcome, my name is Jadiel and I'm a front-end developer</h1>
+      <IconsHome />
     </main>
   );
 }
